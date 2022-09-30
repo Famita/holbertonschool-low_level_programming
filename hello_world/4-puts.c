c57d2c31ae8e481e29ad2e12685312b3ec4cb3d1
Famita <fama.niasse@gmail.com>
2022-09-30 14:27:34 -0700

    all tasks

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,19 +1,12 @@
-/*
- *  * File: 4-puts.c
- *   * Auth: Brennan D Baraban <375@holbertonschool.com>
- *    */
-
 #include <stdio.h>
 
 /**
- *  * main - Prints "Programming is like building a multilingual
- *   *                puzzle, followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ *  * main - this is my first c put
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
 }

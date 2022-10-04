dc8df478d85bb339261f04eb7a0c0334adffef89
Famita <fama.niasse@gmail.com>
2022-10-04 08:22:04 -0700

    task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+   *main- Main function
+    *
+     *Return: Exit status code
+      */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}

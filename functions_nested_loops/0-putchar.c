ed5d067fe35b6f803cf29d44dad284ba71487ad0
Famita <fama.niasse@gmail.com>
2022-10-05 05:41:53 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
-   *main- Main function
-    *
-     *Return: Exit status code
-      */
+ * main - Retry point
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
-	putchar("_putchar");
-	putchar("\n");
-	return (0);
+	char letras[8] = "_putchar";
+
+	int num;
+
+	for (num = 0; num < 8; num++)
+	{
+		_putchar(letras[num]);
+										}
+		_putchar('\n');
+		return (0);
 }

2ff04522931f357924a2c026c1c98cbf1de7178f
Famita <fama.niasse@gmail.com>
2022-10-05 00:57:50 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+   *main- Main function
+    *
+     *Return: Exit status code
+      */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z' ; letter >= 'a' ; letter--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}

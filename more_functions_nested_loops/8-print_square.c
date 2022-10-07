0331bf915c0e1a2cd789be462aff43c3afb93db1
Famita <fama.niasse@gmail.com>
2022-10-07 08:51:47 -0700

    task 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+   *print_diagonal - function that drow a diagonal on the terminal
+    *@n: int
+     *Return: Always 0
+      */
+void print_diagonal(int n)
+{
+	int a = 0;
+	int b = 0;
+
+	if (n > 0)
+	{
+		while (a < n)
+		{
+			while (b < a)
+			{
+				_putchar(' ');
+				b++;
+			}
+			a++;
+			b = 0;
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

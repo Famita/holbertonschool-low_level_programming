39bd1e6b017d7fb4307ce4bda9e2bffa7dc2641a
Famita <fama.niasse@gmail.com>
2022-10-07 08:36:34 -0700

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *more_numbers - function that print numbers 10 times the numbers
+ *
+ *Return: always 0
+ */
+void more_numbers(void)
+{
+	int n;
+	int x;
+	
+	for (x = 0; x < 10; x++)
+	{
+		for (n = 0 ; n <= 14 ; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
